eedf6faf00862fb1dba6cccd4346a43d3f02e418
Almagyar <kotijoszai.simon@gmail.com>
2026-03-18 11:06:26 +0100

    Add files via upload

--- webshop/signup.js
+++ webshop/signup.js
@@ -6,10 +6,15 @@ async function signup() {
         username : newUser.value,
         password : newPass.value
     }
+    if (!newProfile.username || !newProfile.password) {
+        alert("Username and password cannot be empty!");
+        return; 
+    }
 
     await fetch('http://localhost:3000/users', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(newProfile)
       });
+    alert(`Welcome ${newUser.value}!`)
 }
